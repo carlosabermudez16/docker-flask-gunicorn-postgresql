a9cb10131d0ef579a4d75cea11c4ca9614e9d9af
stark_16 <carlosabermudez@unicesar.edu.co>
2022-08-15 18:37:19 -0500

    four commit docker

--- app/database/model.py
+++ app/database/model.py
@@ -2,9 +2,6 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
-from sqlalchemy import Column, String, Integer, Text
-from sqlalchemy import DateTime, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
 from flask_login import UserMixin
 import logging
 
@@ -13,16 +10,16 @@ logging.basicConfig(level=logging.DEBUG)
 class User(UserMixin, db.Model):
     __tablename__ = 'usuarios'
     
-    id = Column(Integer, primary_key = True)
-    username = Column(String(50), unique = True)
-    email = Column(String(40), unique = True)
-    password = Column(Text)
-    public_id = Column(String(50))
-    rol = Column(String(15))
-    comments = relationship('Comment', cascade="delete,merge")
-    created_date = Column(DateTime, default = datetime.now)
-    confirmed = Column(Boolean, nullable=False, default=False)
-    confirmed_on = Column(DateTime, nullable=True)
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True)
+    email = db.Column(db.String(40), unique = True)
+    password = db.Column(db.Text)
+    public_id = db.Column(db.String(50))
+    rol = db.Column(db.String(15))
+    comments = db.relationship('Comment', cascade="delete,merge")
+    created_date = db.Column(db.DateTime, default = datetime.now)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
     
     def __init__(self, username, password, email, public_id):
         self.username = username
@@ -45,10 +42,10 @@ class User(UserMixin, db.Model):
 class Comment(db.Model):
     __tablename__ = 'comments'
 
-    id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('usuarios.id', ondelete="CASCADE"))
-    text = Column(Text())
-    created_date = Column(DateTime, default = datetime.now)
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('usuarios.id', ondelete="CASCADE"))
+    text = db.Column(db.Text)
+    created_date = db.Column(db.DateTime, default = datetime.now)
 
     logging.debug('tabla 2 creada')
 
