d428d9e01d2d8dafb8c0776adb61e2c028cc63b3
stark_16 <carlosabermudez@unicesar.edu.co>
2022-08-15 12:50:40 -0500

    thirds docker

--- app/__init__.py
+++ app/__init__.py
@@ -54,11 +54,11 @@ def create_app(config_class):
     import logging
     engine = create_tables(app=app,config_class=config_class)
     try:
-        db.bind(engine)
-        db.create_all()
+        
+        db.create_all(bind=engine)
         logging.debug('Tablas creadas exitsamente')
     except:
-        logging.debug(f'Error en la creación de la tabla {db.create_all(engine)}')
+        logging.debug(f'Error en la creación de la tabla {db.create_all(bind=engine)}')
 
     from app.models.models import User
 
