df6ae5f4f18cde8da2fa139a0f113c5863c95031
stark_16 <carlosabermudez@unicesar.edu.co>
2022-08-15 19:48:06 -0500

    four commit docker

--- app/database/setup.py
+++ app/database/setup.py
@@ -24,10 +24,10 @@ def create_tables(app,db, config_class):
                 name = 'Postgresql_cloud'
                 print(name)
             elif config_class == 'dev_docker':
-                db.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])                
+                uri =app.config['SQLALCHEMY_DATABASE_URI']              
                 name = 'Postgresql_docker'
                 logging.debug(f"\nConexión a base de datos {name} exitosa!")
-
+            db.create_engine(uri)
             db.create_all()    # crea la tabla en la base de datos que se encuentra en la cadena de conexion(url)                
             #engine.execute("CREATE TABLE IF NOT EXISTS usuarios(id serial, nombre varchar(15), telefono varchar(15))")
             logging.debug('Tablas creada exitosamente!')
@@ -35,5 +35,5 @@ def create_tables(app,db, config_class):
     except:
         
         logging.debug('\nError en la creación de la tabla: ')
-        logging.debug(db.create_all())
+        logging.debug(db.create_engine(uri))
         engine = None
